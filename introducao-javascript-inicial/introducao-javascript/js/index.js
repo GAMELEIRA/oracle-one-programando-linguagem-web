02898bc1314aa1e65c80450ad03cb1e7745f4d1d
GAMELEIRA <gameleira270499@gmail.com>
2020-09-19 22:52:22 -0300

    Adicionando a validacao de calculado de IMC para todos os pacientes

--- introducao-javascript-inicial/introducao-javascript/js/index.js
+++ introducao-javascript-inicial/introducao-javascript/js/index.js
@@ -5,50 +5,194 @@
 
 // console.log(titulo.textContent); //RETORNA O CONTEUDO DE TEXTO
 
-function calcularImc() {
+function buscarPacientes() {
 
-    let pesoValido = true;
+    let pacientes;
 
-    let alturaValida = true;
+    pacientes = document.querySelectorAll(".paciente");
 
-    let paciente = document.querySelector("#primeiro-paciente");
-
-    let tdPeso = paciente.querySelector(".info-peso");
-
-    let tdAltura = paciente.querySelector(".info-altura");
-
-    let tdImc = paciente.querySelector(".info-imc");
-
-    const peso = Number(tdPeso.textContent);
-
-    const altura = Number(tdAltura.textContent);
-
-    if (peso <= 0 || peso >= 1000) {
-
-        pesoValido = false;
-
-        tdImc.textContent = "Peso inválido";
-
-    }
-
-    if (altura <= 0 || altura >= 3.00) {
-
-        alturaValida = false;
-
-        tdImc.textContent = "Altura inválida"
-
-    }
-
-    if (alturaValida && pesoValido) {
-
-        const imc = peso / Math.pow(altura, 2);
-
-        tdImc.textContent = imc;
-
-        console.log(imc);
-
-    }
+    return pacientes;
 
 }
 
-calcularImc();
+function buscarPesoPaciente(paciente) {
+
+    let tdPeso;
+
+    tdPeso = paciente.querySelector(".info-peso");
+
+    return tdPeso;
+
+}
+
+function buscarAlturaPaciente(paciente) {
+
+    let tdAltura;
+
+    tdAltura = paciente.querySelector(".info-altura");
+
+    return tdAltura;
+
+}
+
+function buscarImc(paciente) {
+
+    let tdImc;
+
+    tdImc = paciente.querySelector(".info-imc");
+
+    return tdImc;
+
+}
+
+const validarPeso = (peso) => (peso <= 0 || peso >= 1000) ? false : true;
+
+const validarAltura = (altura) => (altura <= 0 || altura >= 3.00) ? false : true;
+
+const validarCalculoImc = (pesoValido, alturaValida) => (pesoValido && alturaValida) ? true : false;
+
+function calcularImcPaciente(peso, altura) {
+
+    const imc = peso / Math.pow(altura, 2);
+
+    return imc.toFixed(2);
+
+}
+
+function escreverPesoInvalido(peso) {
+
+    peso.textContent = "Peso inválido!";
+
+}
+
+function escreverAlturaInvalida(altura) {
+
+    altura.textContent = "Altura inválida!";
+
+}
+
+function escreverImc(imc, imcCalculado) {
+
+    imc.textContent = imcCalculado;
+
+}
+
+function retornarConteudoTagNumerica(tag) {
+
+    return Number(tag.textContent);
+
+}
+
+function invalidarPaciente(paciente) {
+
+    paciente.classList.add("paciente-invalido");
+
+}
+
+function percorrerTodosPacientes(pacientes) {
+
+    let tdAltura;
+
+    let tdPeso;
+
+    let tdImc;
+
+    let peso;
+
+    let altura;
+
+    let imc;
+
+    let pesoValido;
+
+    let alturaValida;
+
+    let calcularImcPermitido;
+
+    pacientes.forEach(paciente => {
+
+        tdAltura = buscarAlturaPaciente(paciente);
+
+        tdPeso = buscarPesoPaciente(paciente);
+
+        tdImc = buscarImc(paciente);
+
+        altura = retornarConteudoTagNumerica(tdAltura);
+
+        peso = retornarConteudoTagNumerica(tdPeso);
+
+        imc = retornarConteudoTagNumerica(tdImc);
+
+        alturaValida = validarAltura(altura);
+
+        pesoValido = validarPeso(peso);
+
+        calcularImcPermitido = validarCalculoImc(pesoValido, alturaValida);
+
+        if (!alturaValida) {
+
+            escreverAlturaInvalida(tdAltura);
+
+        }
+
+        if (!pesoValido) {
+
+            escreverPesoInvalido(tdPeso);
+
+        }
+
+        if (calcularImcPermitido) {
+
+            imc = calcularImcPaciente(peso, altura);
+
+            escreverImc(tdImc, imc);
+
+        } else {
+
+            invalidarPaciente(paciente);
+
+            escreverImc(tdImc, "IMC INVÁLIDO!");
+
+        }
+
+    })
+
+}
+
+function calcularImcs() {
+
+    const pacientes = buscarPacientes();
+
+    percorrerTodosPacientes(pacientes);
+
+}
+
+calcularImcs();
+
+let titulo = document.querySelector("h1");
+
+
+titulo.addEventListener("click", (event) => {
+
+    event.preventDefault();
+
+
+    console.log("Oi");
+
+})
+
+const botao = document.getElementById("adicionar-paciente");
+
+console.log(botao);
+
+botao.onclick = function (e) {
+
+    alert("OI")
+
+    e.preventDefault();
+
+    console.log("OI");
+
+}
+
+
